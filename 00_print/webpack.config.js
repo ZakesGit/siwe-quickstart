156eeba27ab71a7fc40fb7be9615738de5ff297d
ZakesGit <Zakhelecrypto@gmail.com>
2024-08-09 10:24:32 +0200

    Create webpack.config.js
    creating the front-end for Metamask

---
+++ 00_print/webpack.config.js
@@ -0,0 +1,28 @@
+mkdir siwe-frontend && cd siwe-frontend/
+yarn init --yes
+mkdir src/
+yarn add siwe                   \
+         ethers                 \
+         webpack-node-externals \
+         node-polyfill-webpack-plugin
+         
+yarn add -D html-webpack-plugin \
+            webpack             \
+            webpack-cli         \
+            webpack-dev-server  \
+            bufferutil          \
+            utf-8-validate
+mkdir siwe-frontend && cd siwe-frontend/
+yarn init --yes
+mkdir src/
+yarn add siwe                   \
+         ethers                 \
+         webpack-node-externals \
+         node-polyfill-webpack-plugin
+         
+yarn add -D html-webpack-plugin \
+            webpack             \
+            webpack-cli         \
+            webpack-dev-server  \
+            bufferutil          \
+            utf-8-validate
